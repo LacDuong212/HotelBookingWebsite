8c7ce7ad9b1d8ac7126f7b6084ad4e320f763e5a
aMrnoob <0365946542az@gmail.com>
2025-05-06 20:50:41 +0700

    chỉnh sửa code

--- src/main/java/com/example/hotelbookingwebsite/HotelBookingWebsiteApplication.java
+++ src/main/java/com/example/hotelbookingwebsite/HotelBookingWebsiteApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelBookingWebsiteApplication {
-
+    //CHinh sửa code
     public static void main(String[] args) {
         SpringApplication.run(HotelBookingWebsiteApplication.class, args);
     }
